80977056905d56e90dd8bc9fa7e6041d6306cf42
john <kyle3278@msn.com>
2018-12-12 09:51:21 +0000

    add captcha

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -9,7 +9,16 @@ use App\Http\Requests\Api\VerificationCodeRequest;
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms){
-        $phone=$request->phone;
+        $captchaData=\Cache::get($request->captcha_key);
+        if(!$captchaData){
+            return $this->response->error('图片验证码已失效',422);
+        }
+        if(!hash_equals($captchaData['code'],$request->captcha_code)){
+            // 验证错误就清除缓存
+            \Cache::forget($request->captcha_key);
+            return $this->response->errorUnauthorized('验证码错误');
+        }
+        $phone=$captchaData['phone'];;
 
 //        除了正式环境外，其他环境，默认不真实发送短信，短信验证码默认为1234
         if (!app()->environment('production')) {
@@ -24,18 +33,23 @@ class VerificationCodesController extends Controller
                 ]);
             } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
                 $message = $exception->getException('yunpian')->getMessage();
-                return $this->response->errorInternal($message ?: '短信发送异常');
+                return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
 
         $key='verificationCode_'.str_random(15);
         $expiredAt=now()->addMinutes(10);
-        
+
+        // 缓存验证码 10分钟过期
         \Cache::put($key,['phone'=>$phone, 'code'=>$code], $expiredAt);
+        // 清除图片验证码缓存
+        \Cache::forget($request->captcha_key);
+
         return $this->response->array([
             'key'=>$key,
             'code'=>$code,
             'expired_at'=>$expiredAt->toDateTimeString(),
+
         ])->setStatusCode(201);
 
     }
