6190e8e4390f2d9e2e357efaa8cf7186f1e072df
john <kyle3278@msn.com>
2018-12-12 08:36:20 +0000

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,18 @@ use Illuminate\Http\Request;
     $api->version('v1',[
         'namespace'=>'App\Http\Controllers\Api'
     ],function($api){
+
+        $api->group([
+            'middleware'=>'api.throttle',
+            'limit'=>config('api.rate_limit.sign.limit'),
+            'expires'=>config('api.rate_limit.sign.expires'),
+        ], function($api){
         // 短信验证码
         $api->post('verificationCodes', 'VerificationCodesController@store')
             ->name('api.verificationCodes.store');
         // 用户注册
         $api->post('users', 'UsersController@store')
-            ->name('api.users.store');
+            ->name('api.users.store');    
+        });
+        
     });
